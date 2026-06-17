611b1d335a736b1732a29d7394c4556cf2470983
Sakshi <sakshi.jun04@gmail.com>
2026-06-17 23:10:49 +0530

    feat : Impelemented Enums for Parking Lot System

---
+++ Parking_Lot/Enums/SpotStatus.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking_Lot.Enums
+{
+    public enum SpotStatus
+    {
+        Free,
+        Occupied
+    }
+}
---
+++ Parking_Lot/Enums/VehicleType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking_Lot.Enums
+{
+    public enum VehicleType
+    {
+       Bike,
+        Car,
+        Truck
+    }
+}
