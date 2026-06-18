276dc695aaaf34335a3d0af2eea7cd28778258a6
Sakshi <sakshi.jun04@gmail.com>
2026-06-18 16:30:14 +0530

    feat : Implemented Parking Ticket

---
+++ Parking_Lot/Models/ParkingTicket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking_Lot.Models
+{
+    public class ParkingTicket
+    {
+        public Guid TicketId { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public ParkingSpot ParkingSpot { get; set; }
+        public DateTime EntryTime { get; set; }
+        public ParkingTicket(Vehicle vehicle, ParkingSpot parkingSpot)
+        {
+            TicketId = Guid.NewGuid();
+            Vehicle = vehicle;
+            ParkingSpot = parkingSpot;
+            EntryTime = DateTime.Now;
+        }
+    }
+}
