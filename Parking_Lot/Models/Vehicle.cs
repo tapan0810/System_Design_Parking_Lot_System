9be587d544905647268b285bca1ade722c14f9fa
Sakshi <sakshi.jun04@gmail.com>
2026-06-18 15:06:33 +0530

    feat : Implemented Vehicle.cs

---
+++ Parking_Lot/Models/Vehicle.cs
@@ -0,0 +1,19 @@
+﻿using Parking_Lot.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking_Lot.Models
+{
+    public abstract class Vehicle
+    {
+        public string LicenseNumber { get; set; }
+        public VehicleType VehicleType { get; set; }
+
+        protected Vehicle(string licenseNumber, VehicleType vehicleType)
+        {
+            LicenseNumber = licenseNumber;
+            VehicleType = vehicleType;
+        }
+    }
+}
