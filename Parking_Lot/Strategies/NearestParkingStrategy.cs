62370eb9662c36d632b4c530d74721aeb6b22dde
Sakshi <sakshi.jun04@gmail.com>
2026-06-18 16:42:22 +0530

    feat: Implemented ParkingStrategy for Farthest and Closest spots

---
+++ Parking_Lot/Strategies/NearestParkingStrategy.cs
@@ -0,0 +1,16 @@
+﻿using Parking_Lot.Interfaces;
+using Parking_Lot.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking_Lot.Strategies
+{
+    public class NearestParkingStrategy : IParkingStrategy
+    {
+        public ParkingSpot FindSpot(List<ParkingSpot> spots)
+        {
+            return spots.FirstOrDefault(spot => spot.IsFree())!;
+        }
+    }
+}
