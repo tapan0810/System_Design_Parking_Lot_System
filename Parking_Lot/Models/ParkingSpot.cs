2100426dd70b11a0872050ad9d78e46ec3e4a15b
Sakshi <sakshi.jun04@gmail.com>
2026-06-18 16:23:48 +0530

    feat : Implemented ParkingSpot

---
+++ Parking_Lot/Models/ParkingSpot.cs
@@ -0,0 +1,38 @@
+﻿using Parking_Lot.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking_Lot.Models
+{
+    public class ParkingSpot
+    {
+        public int SpotId { get; set; }
+        public SpotType SpotType { get; set; }
+        public SpotStatus SpotStatus { get; set; }
+        public Vehicle vehicle { get; set; }
+        public ParkingSpot(int spotId, SpotType spotType)
+        {
+            SpotId = spotId;
+            SpotType = spotType;
+            SpotStatus = SpotStatus.Free;
+        }
+
+        public bool IsFree()
+        {
+            return SpotStatus == SpotStatus.Free;
+        }
+
+        public void AssignVehicle(Vehicle vehicle)
+        {
+            this.vehicle = vehicle;
+            SpotStatus = SpotStatus.Occupied;
+        }
+
+        public void RemoveVehicle()
+        {
+            vehicle = null;
+            SpotStatus = SpotStatus.Free;
+        }
+    }
+}
