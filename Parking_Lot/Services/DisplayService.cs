279fa5259bc3d7787ceb83d8dac776325f21b5b9
Sakshi <sakshi.jun04@gmail.com>
2026-06-25 22:02:46 +0530

    feat : Implemented DisplayService

---
+++ Parking_Lot/Services/DisplayService.cs
@@ -0,0 +1,25 @@
+﻿using Parking_Lot.Enums;
+using Parking_Lot.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Parking_Lot.Services
+{
+    public class DisplayService
+    {
+        public void UpdateDisplay(List<ParkingSpot> spots)
+        {
+            Console.WriteLine();
+
+            Console.WriteLine("Display Board");
+
+            foreach (var group in spots.GroupBy(x => x.SpotType))
+            {
+                Console.WriteLine($"{group.Key} => {group.Count(x => x.SpotStatus == SpotStatus.Free)}");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
